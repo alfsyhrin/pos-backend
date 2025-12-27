bb66c2b12212b68bf80488e077dafac5d2bed0bb
alfsyhrin <alfisyahrinah87@gmail.com>
2025-12-27 19:31:02 +0700

    test

--- src/routes/backup.routes.js
+++ src/routes/backup.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const backupController = require('../controllers/backup.controllers');
 const authMiddleware = require('../middleware/auth');
 const checkTenant = require('../middleware/checkTenant');
 const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() }); // untuk upload file JSON
+
+const backupController = require('../controllers/backup.controllers');
 
 router.get('/export', authMiddleware(), checkTenant, backupController.exportData);
 router.post('/import', authMiddleware(), checkTenant, upload.single('file'), backupController.importData);
